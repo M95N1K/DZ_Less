2c950543f2799cc579d35864840e43f72fb51804
M95N1K <mysnik999@gmail.com>
2020-05-03 13:29:22 +0600

    Готово задание 1

--- DZ_Less/Program.cs
+++ DZ_Less/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VClass;
+
 
 namespace DZ_Less2_1
 {
@@ -19,7 +15,7 @@ namespace DZ_Less2_1
         }
         static void Main(string[] args)
         {
-            Cons.Print(MinX());
+            Console.WriteLine(MinX());
         }
     }
 }
