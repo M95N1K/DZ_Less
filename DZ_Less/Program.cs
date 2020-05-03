4ab427f5a087bbef503a8eae40d42ff7dab237d0
M95N1K <mysnik999@gmail.com>
2020-05-03 13:22:28 +0600

    Задача_1

--- DZ_Less/Program.cs
+++ DZ_Less/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VClass;
 
-namespace DZ_Less
+namespace DZ_Less2_1
 {
     class Program
     {
+        static int MinX(int a=3, int b=7, int c=5)
+        {
+            if (a < b)
+                b = a;
+            if (b < c)
+                c = b;
+            return c;
+        }
         static void Main(string[] args)
         {
+            Cons.Print(MinX());
         }
     }
 }
