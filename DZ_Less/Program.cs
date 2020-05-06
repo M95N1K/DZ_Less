a6bb481559bf61c9405dba7b1f8cb40020387ff3
M95N1K <mysnik999@gmail.com>
2020-05-06 15:21:22 +0600

    Задание 1 доработка до swich

--- DZ_Less/Program.cs
+++ DZ_Less/Program.cs
@@ -72,19 +72,54 @@ namespace DZ_Less3_1
         {
             Complex ComplexA = new Complex(12, 15);
             Complex ComplexB = new Complex(3, 7);
-            Complex result = new Complex();
+            Complex result;
+            bool key;
+            
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Число А: {0}", ComplexA.ToString());
+                Console.WriteLine("Число B: {0}", ComplexB.ToString());
+                Console.Write("Введите номер действия: \n 1 -Сложение\n 2 -Вычитание\n 3 -Умножение\n 4 -Диление\n");
+                int oper;
+                key = false;
+                do
+                {
+                    key = int.TryParse(Console.ReadLine(), out oper);
+                    if (!key)
+                        Console.WriteLine("Неверный ввод");
+                }
+                while (!key);
+                key = false;
+                
+                switch (oper)
+                {
+                    case 1:
+                        result = ComplexA.ComplexSum(ComplexB);
+                        Console.WriteLine("Результат сложения: {0}", result.ToString());
+                        break;
+                    case 2:
+                        result = ComplexA.ComplexSubtraction(ComplexB);
+                        Console.WriteLine("Результат вычитания: {0}", result.ToString());
+                        break;
+                    case 3:
+                        result = ComplexA.ComplexMultiplication(ComplexB);
+                        Console.WriteLine("Результат умножения: {0}", result.ToString());
+                        break;
+                    case 4:
+                        result = ComplexA.ComplexDivision(ComplexB);
+                        Console.WriteLine("Результат деления: {0}", result.ToString());
+                        break;
+                    default:
+                        key = true;
+                        break;
+                }
+            } while (key);
 
-            Console.WriteLine("Число А: {0}", ComplexA.ToString());
-            Console.WriteLine("Число B: {0}", ComplexB.ToString());
-
-            result = ComplexA.ComplexSum(ComplexB);
-            Console.WriteLine("Результат сложения: {0}", result.ToString());
-            result = ComplexA.ComplexSubtraction(ComplexB);
-            Console.WriteLine("Результат вычитания: {0}", result.ToString());
-            result = ComplexA.ComplexMultiplication(ComplexB);
-            Console.WriteLine("Результат умножения: {0}", result.ToString());
-            result = ComplexA.ComplexDivision(ComplexB);
-            Console.WriteLine("Результат деления: {0}", result.ToString());
+            
+            
+            
+            
 
             Console.ReadLine();
         }
